307092961b7d7d41885440a8d3033eb566849604
zheng-jj <zhengjiongjie12345@gmail.com>
2021-12-09 04:08:27 +0800

    added generated documentation from pydoc

--- classes/game.py
+++ classes/game.py
@@ -8,4 +8,7 @@ class Game:
         self.turn_num = 1
 
     def print_turn_num(self):
+        """
+        prints turn number based on self.turn_num in game class
+        """
         print("Turn {}".format(self.turn_num))
