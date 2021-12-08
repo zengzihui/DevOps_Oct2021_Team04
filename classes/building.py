a2ecdf95da89e6ab033ebf84af4e6a8adbd456b1
zheng-jj <zhengjiongjie12345@gmail.com>
2021-12-09 05:53:13 +0800

    added comments

--- classes/building.py
+++ classes/building.py
@@ -1,5 +1,8 @@
 class Building:
     def __init__(self):
+        """
+        init function for building class
+        """
         self.name = ""
         self.x_coord = None
         self.y_coord = None
