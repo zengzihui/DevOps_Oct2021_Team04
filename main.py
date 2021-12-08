662b2f854d00ac0264caa68660aebcf0f4d22986
zheng-jj <zhengjiongjie12345@gmail.com>
2021-12-09 02:35:04 +0800

    create function to exit program and program to call this function when user enters '0' in main menu

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from classes.menu import *
 def main():
     while True:
         chosen_option = main_menu()
+        if chosen_option == "0":
+            exit_game()
 
 
 if __name__ == "__main__":
