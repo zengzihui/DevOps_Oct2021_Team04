c06e3091efa9d62638f2268a6c7241710b897ca0
zheng-jj <64000939+zheng-jj@users.noreply.github.com>
2021-12-09 06:18:47 +0800

    Implement Print Game Board user story (#39)
    * add unit test for printing game board
    
    * add superclass building, updated game intiation to create empty game board and add function to print game board
    
    * added comments

---
+++ classes/building.py
@@ -0,0 +1,9 @@
+class Building:
+    def __init__(self):
+        """
+        init function for building class
+        """
+        self.name = ""
+        self.x_coord = None
+        self.y_coord = None
+        return
--- classes/game.py
+++ classes/game.py
@@ -1,14 +1,54 @@
+from .building import Building
+
+
 class Game:
 
-    def __init__(self):
+    def __init__(self, height=3, width=3):
         """
         init function for game class
         default turn number is 1
         """
+        self.height = height
+        self.width = width
+        self.board = []
         self.turn_num = 1
+
+        width_counter = 0
+        while (width_counter <= width):
+            height_counter = 0
+            self.board.append([])
+
+            while (height_counter <= height):
+                self.board[width_counter].append(Building())
+                height_counter += 1
+
+            width_counter += 1
 
     def print_turn_num(self):
         """
         prints turn number based on self.turn_num in game class
         """
         print("Turn {}".format(self.turn_num))
+
+    def print_board(self):
+        """
+        Print the map board with the building names
+        """
+        game_board_string = " "
+        row_count = 0
+        for header in range(0, self.width + 1):
+            game_board_string += '   {:1s}  '.format(chr(header + 65))
+        game_board_string += "\n"
+        for row in self.board:
+            game_board_string += "{:26s}".format(" +-----+-----+-----+-----+") + "\n"
+            game_board_string += str(row_count + 1)
+            for building in row:
+                building_short = ""
+                if building:
+                    building_short = building.name
+                game_board_string += '|'
+                game_board_string += ' {:3s} '.format(building_short)
+            game_board_string += "|\n"
+            row_count += 1
+        game_board_string += "{:26s}".format(" +-----+-----+-----+-----+")
+        print(game_board_string)
