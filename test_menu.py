92dd074b38eb7d09b76faeb0d7bc58059db65667
zheng-jj <zhengjiongjie12345@gmail.com>
2021-12-09 10:32:26 +0800

    found code missing tests, added new unit test to cover

--- test_menu.py
+++ test_menu.py
@@ -24,6 +24,25 @@ def test_main_menu_display(mocker, option_list):
     assert out[0] == 'Welcome, mayor of Simp City!\n----------------------------\n1. Start new game\n2. Load saved game\n\n0. Exit'
 
 
+@pytest.mark.parametrize("option_list",
+                         [(["0"])])
+def test_main_menu_display_without_welcome(mocker, option_list):
+    """
+    test if main menu options are printed correctly
+    """
+    set_keyboard_input([None, None])
+
+    # set input for menu options
+    mocker.patch('builtins.input', side_effect=option_list)
+
+    # run main menu function
+    main_menu(True)
+
+    # check if main_menu function displays correct output
+    out = get_display_output()
+    assert out[0] == '\n1. Start new game\n2. Load saved game\n\n0. Exit'
+
+
 @pytest.mark.parametrize("option_list, expected",
                          [(["2"], "2"), (["1"], "1"), (["0"], "0")])
 def test_main_menu_input_success(mocker, option_list, expected):
@@ -68,6 +87,7 @@ def test_start_new_game(mocker):
     """
     mocker.patch('classes.game.Game.start_new_turn', return_value="new turn started")
     assert start_new_game() == "new turn started"
+
 
 def test_main_menu_exit_program():
     """
