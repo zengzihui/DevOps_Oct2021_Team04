21c51f7172d5ae8f6434edcd0c1ee45acae5870a
Jian Oon Swah <s10195746@connect.np.edu.sg>
2022-01-17 03:42:28 +0800

    updated unit test and finished shop calculation

--- classes/shop.py
+++ classes/shop.py
@@ -7,5 +7,61 @@ class Shop(Building):
 
     Swah Jian Oon T01 9th December
     """
-    def __init__(self):
+    def __init__(self,x_coord,y_coord):
         self.name = "SHP"
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+
+    def calculate_score(self,game_board):
+        """
+        This function calculates the score for a shop object on the game board
+        """
+        total_score = 0
+        score_dict = {"":0,self.name:0}
+        # fill up score_dict dictionary with 4 different types of building with 1 count
+        for key in game_board.building_pool:
+            if key != "SHP":
+                score_dict[key] = 1
+                continue
+        
+        temp_x_lower = self.x_coord - 1
+        if (0 <= temp_x_lower < game_board.width):
+            pass
+        else:
+            temp_x_lower = 0
+
+        temp_x_higher = self.x_coord + 1
+        if (0 <= temp_x_higher < game_board.width):
+            pass
+        else:
+            temp_x_higher = game_board.width
+
+        temp_y_lower = self.y_coord - 1
+        if (0 <= temp_y_lower < game_board.height):
+            pass
+        else:
+            temp_x_lower = 0
+        
+        temp_y_higher = self.y_coord + 1
+        if (0 <= temp_y_higher < game_board.height):
+            pass
+        else:
+            temp_x_higher = game_board.height
+        
+        if score_dict[game_board.board[self.y_coord][temp_x_lower].name] == 1:
+            score_dict[game_board.board[self.y_coord][temp_x_lower].name] = 0
+            total_score += 1
+        
+        if score_dict[game_board.board[self.y_coord][temp_x_higher].name] == 1:
+            score_dict[game_board.board[self.y_coord][temp_x_higher].name] = 0
+            total_score += 1
+
+        if score_dict[game_board.board[temp_y_lower][self.x_coord].name] == 1:
+            score_dict[game_board.board[temp_y_lower][self.x_coord].name] = 0
+            total_score += 1
+        
+        if score_dict[game_board.board[temp_y_higher][self.x_coord].name] == 1:
+            score_dict[game_board.board[temp_y_higher][self.x_coord].name] = 0
+            total_score += 1
+
+        return total_score
--- test_shop.py
+++ test_shop.py
@@ -9,35 +9,49 @@ from classes.factory import *
 from classes.beach import *
 from classes.game import *
 
-@pytest.mark.parametrize("game_board,score",
-                         [
+@pytest.mark.parametrize("game_board,score,x_coord,y_coord",
+                         [  
                              ([
-                                 [Shop()]
+                                 [Shop(0,0),Building(),Building(),Building()],
+                                 [Building(),Building(),Building(),Building()],
+                                 [Building(),Building(),Building(),Building()],
+                                 [Building(),Building(),Building(),Building()]
                             
-                                 ],0), 
+                                 ],0,0,0), 
+
                              ([
-                                 [Shop(), Highway()]
-                                 ],1), 
+                                 [Shop(0,0), Highway(), Building(), Building()],
+                                 [Building(),Building(),Building(),Building()],
+                                 [Building(),Building(),Building(),Building()],
+                                 [Building(),Building(),Building(),Building()]
+
+                                 ],1,0,0), 
                              (
                                  [
-                                     [House(), Shop(),Highway()]
-                                 ],2),
+                                     [House(), Shop(1,0),Highway(),Building()],
+                                     [Building(),Building(),Building(),Building()],
+                                     [Building(),Building(),Building(),Building()],
+                                     [Building(),Building(),Building(),Building()]
+                                 ],2 ,1,0),
                              (
                                  [
-                                     [House(), Shop(),Highway()],
-                                     [None,Beach(),None]
-                                 ],3),
+                                     [House(), Shop(1,0),Highway(),Building()],
+                                     [Building(),Beach(),Building(),Building()],
+                                     [Building(),Building(),Building(),Building()],
+                                     [Building(),Building(),Building(),Building()]
+                                 ],3, 1,0 ),
                              (
                                  [
-                                     [None, Beach(),None],
-                                     [House(),Shop(),Highway()],
-                                     [None,Factory(),None]
-                                 ],4),
+                                     [Building(),Beach(),Building(),Building()],
+                                     [House(),Shop(1,1),Highway(),Building()],
+                                     [Building(),Factory(),Building(),Building()],
+                                     [Building(),Building(),Building(),Building()]
+                                 ],4, 1, 1),
                          
                          ])
-def test_calculate_score(game_board, score):
+def test_calculate_score(game_board, score, x_coord, y_coord):
 
-    test_building = Shop()
+    test_building = Shop(x_coord, y_coord)
     test_game = Game()
     test_game.board = game_board
-    assert test_building.calculate_score(test_game.board) == score
+    assert test_building.calculate_score(test_game) == score
