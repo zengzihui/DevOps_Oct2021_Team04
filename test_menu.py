4b7616532eaa48b6ecc527bc54c4d6fbc043648a
zheng-jj <zhengjiongjie12345@gmail.com>
2022-01-18 23:18:41 +0800

    added uncovered cases in unit tests

--- test_menu.py
+++ test_menu.py
@@ -122,7 +122,7 @@ def test_main_menu_exit_program():
 
 @pytest.mark.parametrize("option_list, expected",
                          [(["5", "5"], [5, 5]), (["5", "6"], [5, 6]), (["5", "5"], [5, 5]), (["1", "40"], [1, 40]),
-                          (["40", "40"], [4, 4])])
+                          (["40", "40"], [4, 4]), (["a", "b"], [4, 4]), (["-4", "40"], [4, 4])])
 def test_choose_city_size(mocker, option_list, expected):
     """
     test if prompt_city_size() returns back valid city sizes
