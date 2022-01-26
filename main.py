3c4f1c1ac04b46ceaa3ef54850b8081110332dbe
Jian Oon Swah <s10195746@connect.np.edu.sg>
2022-01-27 03:15:14 +0800

    Implement Display High Score

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
         elif chosen_option == "1":
             start_new_game(width=city_size[0], height=city_size[1])
         elif chosen_option == "3":
-            Game(city_size[0],city_size[1]).display_high_score()
+            Game(city_size[0],city_size[1]).display_high_score() 
         elif chosen_option == "5":
             city_size = prompt_city_size(city_size)
 
